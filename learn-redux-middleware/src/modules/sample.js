394a413a5854de2441e615fc10f732ab9e6fabc3
YoujungSon <andn1026@gmail.com>
2023-02-07 17:29:53 +0900

    redux-saga

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,25 +1,29 @@
-import { handleActions } from 'redux-actions';
+import { handleActions, createAction } from 'redux-actions';
 import * as api from '../lib/api';
-import createRequestThunk from '../lib/createRequestThunk';
+import { finishLoading, startLoading } from './loading';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga from './../lib/createRequestSaga';
 
 // 액션 타입을 선언한다.
-// 한 요청당 세 개를 만ㄷ늘어야 한다.
-
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 
-// thunk 함수를 생성한다.
-// thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치한다.
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
 
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 // 초기 상태를 선언한다.
-// 요청의 로딩 중 상태는 loading이라는 객체에서 관리한다.
+// 요청의 로딩 중 상태는 loading 이라는 객체에서 관리한다.
 
 const initialState = {
   post: null,
@@ -30,12 +34,10 @@ const sample = handleActions(
   {
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
-
       post: action.payload,
     }),
     [GET_USERS_SUCCESS]: (state, action) => ({
       ...state,
-
       users: action.payload,
     }),
   },
