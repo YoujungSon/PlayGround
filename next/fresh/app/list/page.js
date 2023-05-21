5a2975489ea9481d27faa83a8a562e7cf27fce06
YoujungSon <andn1026@gmail.com>
2023-05-21 15:44:36 +0900

    좋아요 버튼 만들기 (useState, onClick)

--- next/fresh/app/list/page.js
+++ next/fresh/app/list/page.js
@@ -1,5 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function List() {
   let 상품 = ['Tomatoes', 'Pasta', 'Cocount'];
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h4 className='title'>상품목록</h4>
@@ -8,6 +13,21 @@ export default function List() {
           <div className='food' key={a}>
             <img src={`/food${i}.png`} alt='토마토' className='food-img' />
             <h4>{a} $40</h4>
+            <button
+              onClick={() => {
+                setCount(count - 1);
+              }}
+            >
+              -
+            </button>
+            <span>{count}</span>
+            <button
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              +
+            </button>
           </div>
         );
       })}
