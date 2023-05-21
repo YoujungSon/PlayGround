f73458fc96521fd294259087669a0d5a806258f3
YoujungSon <andn1026@gmail.com>
2023-05-21 15:08:01 +0900

    Component에 데이터 전해주려면 props

--- next/fresh/app/cart/page.js
+++ next/fresh/app/cart/page.js
@@ -1,21 +1,28 @@
 import age from './data';
 import Hi from './hi';
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'Pasta'];
   return (
     <div>
       <h4 className='title'>Cart {age}</h4>
       <Hi />
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      <CartItem item={장바구니[0]} />
+      <CartItem item={장바구니[1]} />
+      <Banner content='롯데카드' />
+      <RedButton color={'blue'} />
     </div>
   );
 }
-
-function CartItem() {
+function Banner(props) {
+  return <h5>{props.content} 결제 행사중</h5>;
+}
+function RedButton(props) {
+  return <button style={{ backgroundColor: `${props.color}` }}>버튼</button>;
+}
+function CartItem(props) {
   return (
     <div className='cart-item'>
-      <p>상품명</p>
+      <p>{props.item}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
